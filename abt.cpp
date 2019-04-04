626c39c317fc8fb76d62a32f41663a9a5fb1da12
viralsinha12 <viral.sinha@hotmail.com>
2019-04-03 23:05:53 -0400

    Code changed as per ABT's FSM

--- abt.cpp
+++ abt.cpp
@@ -26,6 +26,9 @@ using namespace std;
 //
 
 int expectedSeq;
+int recentlyAckSeq;
+int count;
+
 int seqNum = 0;
 int ackNum = 0;
 int sentFlag = 0;
@@ -59,12 +62,6 @@ void A_output(struct msg message)
 	}
 
 	if(sentFlag == 0){
-		// cout<<endl;
-		// cout<<"Number of messages sent : "<<messagessent++<<endl;
-		// cout<<"A_output Start : "<<endl;
-		// cout<<"Sent message from A_output : "<<newPacket.payload<<endl;
-		// cout<<"Sent seqnum from A_output : "<<newPacket.seqnum<<endl;
-		// cout<<"A_output End : "<<endl;
 		tolayer3(0,newPacket);
 		starttimer(0,timeOutValue);
 		sentFlag = 1;
@@ -73,35 +70,21 @@ void A_output(struct msg message)
 	}
 	else
 	{
-		// cout<<endl;
-		// cout<<"Number of messages sent : "<<messagessent++<<endl;
-		// cout<<"Buffer Start : "<<endl;
-		// cout<<"Buffer Sent message from A_output : "<<newPacket.payload<<endl;
-		// cout<<"Buffer Sent seqnum from A_output : "<<newPacket.seqnum<<endl;
-		// cout<<"Buffer End : "<<endl;
 		bufferQueue.push(newPacket);
-		// cout<<"Queue Size : "<<bufferQueue.size()<<endl;
 	}
 }
 
 /* called from layer 3, when a packet arrives for layer 4 */
 void A_input(struct pkt packet)
 {
-	stoptimer(0);
+	
 	if(packet.seqnum == recentlySentSeqNum)
 	{
-		// cout<<endl;
-		// cout<<"--------Acknowledgment Start------"<<endl;
-		// cout<<"Received Packet Sequence Number : "<<packet.seqnum<<endl;
-		// cout<<"Received Packet Payload : "<<packet.payload<<endl;
-		// cout<<"Current Queue Size : "<<bufferQueue.size()<<endl;
-		// cout<<"--------Acknowledgment End------"<<endl;
+		stoptimer(0);
 		if(!bufferQueue.empty())
 		{
 			sentFlag = 0;
 			tolayer3(0,bufferQueue.front());
-			// cout<<"--------Message sent after Acknowledgment : "<<bufferQueue.front().payload<<endl;
-			// cout<<"--------Message seqnum after Acknowledgment : "<<bufferQueue.front().seqnum<<endl;
 			starttimer(0,timeOutValue);
 			sentFlag = 1;
 			recentlySentSeqNum = bufferQueue.front().seqnum;
@@ -110,31 +93,11 @@ void A_input(struct pkt packet)
 		else
 			sentFlag = 0;	
 	}
-	else
-	{
-		//cout<<endl;
-		// cout<<"--------Negative Acknowledgment Start------"<<endl;
-		// cout<<"Received Packet Sequence Number : "<<packet.seqnum<<endl;
-		// cout<<"Received Packet Payload : "<<packet.payload<<endl;
-		// cout<<"--------Negative Acknowledgment End------"<<endl;
-		tolayer3(0,recentlySentPacket);
-		starttimer(0,timeOutValue);
-		sentFlag = 1;
-		recentlySentSeqNum = recentlySentPacket.seqnum;
-		recentlySentPacket = recentlySentPacket;	
-	}
 }
 
 /* called when A's timer goes off */
 void A_timerinterrupt()
 {
-		// cout<<endl;
-		// cout<<"interruptCount : "<<interruptCount++<<endl;
-		// cout<<"--------Interrupt Start------"<<endl;
-		// cout<<"Interrupt Packet Sequence Number : "<<recentlySentPacket.seqnum<<endl;
-		// cout<<"Interrupt Packet Payload : "<<recentlySentPacket.payload<<endl;
-		// cout<<"--------Interrupt End------"<<endl;
-
 		tolayer3(0,recentlySentPacket);
 		starttimer(0,timeOutValue);
 		sentFlag = 1;
@@ -154,13 +117,15 @@ void A_init()
 /* called from layer 3, when a packet arrives for layer 4 at B*/
 void B_input(struct pkt packet)
 {
+	count = count+1;
 	int checkSum = 0;
-	// cout<<endl;
-	// cout<<"****B_Input****"<<endl;
-	// cout<<"expectedSeq : "<<expectedSeq <<endl;
+
+	if(count>1 && packet.seqnum == recentlyAckSeq)
+	{
+		tolayer3(1,packet);
+	}
 	if(packet.seqnum == expectedSeq)
 	{
-		// cout<<"Received seqnum : "<<expectedSeq <<endl;
 		for(int i=0;i<20;i++)
 		{
 			checkSum = checkSum + int(packet.payload[i]);
@@ -168,7 +133,8 @@ void B_input(struct pkt packet)
 		checkSum = checkSum + packet.seqnum + packet.acknum;
 		if(checkSum == packet.checksum)
 		{
-			// cout<<"______________________Proper Message____________________"<<endl;
+
+			recentlyAckSeq = packet.seqnum;
 			tolayer5(1,packet.payload);
 
 			if (expectedSeq == 0)
@@ -180,18 +146,8 @@ void B_input(struct pkt packet)
 		}
 		else
 		{
-			// cout<<"ImProper Message"<<endl;
-
-			int seqack = 0;
-
-			if(expectedSeq == 1)
-				seqack = 0;
-			else
-				seqack = 1;
-
-			packet.seqnum = seqack;
-			packet.acknum = seqack;
-			tolayer3(1,packet);
+			packet.seqnum = recentlyAckSeq;
+			tolayer3(1,packet);	
 		}
 	}
 }
@@ -200,5 +156,7 @@ void B_input(struct pkt packet)
 /* entity B routines are called. You can use it to do any initialization */
 void B_init()
 {
-	int expectedSeq = 0;
+	expectedSeq = 0;
+	recentlyAckSeq = 0;
+	count = 0;
 }
